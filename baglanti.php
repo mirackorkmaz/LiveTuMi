d5e069cb9cd2a19ae209b9253b398191891eb068
Miraç Korkmaz <mirackorkmaz@outlook.com>
2025-04-23 20:48:02 +0300

    Anasayfanın güncellenmesi

---
+++ baglanti.php
@@ -0,0 +1,16 @@
+<?php
+
+$vt_sunucu = "localhost";
+$vt_kullanici = "root";
+$vt_sifre = "";
+$vt_adi = "livetumi";
+
+$baglanti = mysqli_connect($vt_sunucu, $vt_kullanici, $vt_sifre, $vt_adi);
+
+if (!$baglanti) {
+    die("Vertitabani baglanti islemi basarisiz".mysqli_connect_error());
+}
+else {
+    echo "Baglanti basarili.";
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include("baglanti.php");
+    include("kayit.php");
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>LiveTuMi</title>
+</head>
+<body>
+    
+</body>
+</html>
